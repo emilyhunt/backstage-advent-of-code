ba019019eb4558d552fac929536e344ae9b89201
Michael Otty <michael.otty@gmail.com>
2021-12-01 18:43:55 +0000

    Add part solution to day 1 part 1

--- michael/01/01.cpp
+++ michael/01/01.cpp
@@ -4,6 +4,12 @@
 #include <sstream>
 #include <vector>
 
+std::vector<int> ReadFile(const char *fileName);
+void PrintNumbers(const std::vector<int> &numbers);
+int Part1(const std::vector<int> &numbers);
+int Sum(const std::vector<int> &numbers);
+
+
 std::vector<int> ReadFile(const char *fileName)
 {
     int i;
@@ -16,7 +22,7 @@ std::vector<int> ReadFile(const char *fileName)
     return numbers;
 }
 
-void PrintNumbers(const std::vector<int>& numbers)
+void PrintNumbers(const std::vector<int> &numbers)
 {
     bool first = true;
     for (int number : numbers)
@@ -31,10 +37,29 @@ void PrintNumbers(const std::vector<int>& numbers)
     std::cout << "\n";
 }
 
+int Part1(const std::vector<int> &numbers)
+{
+    std::vector<int> diffs;
+    for (int i = 0; i < numbers.size() - 1; i++)
+    {
+        if ((numbers[i + 1] - numbers[i]) > 0)
+            diffs.push_back(1);
+    }
+    return Sum(diffs);
+}
+
+int Sum(const std::vector<int> &numbers)
+{
+    int sum = 0;
+    for (int i : numbers)
+        sum += i;
+    return sum;
+}
+
 int main()
 {
     std::vector<int> numbers = ReadFile("01/data/input.txt");
-    PrintNumbers(numbers);
+    std::cout << Part1(numbers) << std::endl;
 
     return 0;
 }
