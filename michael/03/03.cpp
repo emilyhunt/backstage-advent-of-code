75446ac0d7e04c97e997f0b73741057901008569
Michael Otty <michael.otty@gmail.com>
2021-12-04 21:39:10 +0000

    Add helper functions for part 2

--- michael/03/03.cpp
+++ michael/03/03.cpp
@@ -7,19 +7,32 @@
 
 #include "Utilities/Utilities.h"
 
+enum class Rate
+{
+    Epsilon,
+    Gamma
+};
+enum class Rating
+{
+    OxygenGenerator,
+    Co2Scrubber
+};
+
 int Part1(const std::vector<std::string> &lines);
 int Part2(const std::vector<std::string> &lines);
 std::vector<std::string> ExtractLines(const std::string &text);
-int CalculateRate(const std::vector<std::string> &lines, const std::string& option);
+int BinaryToDecimal(const std::vector<unsigned int>& binaryVector);
+int CalculateRate(const std::vector<std::string> &lines, Rate option);
+int CalculateRating(const std::vector<std::string> &lines, Rating option);
 
 int Part1(const std::vector<std::string> &lines)
 {
-    return CalculateRate(lines, "gamma") * CalculateRate(lines, "epsilon");
+    return CalculateRate(lines, Rate::Gamma) * CalculateRate(lines, Rate::Epsilon);
 }
 
 int Part2(const std::vector<std::string> &lines)
 {
-    return 0;
+    return CalculateRating(lines, Rating::OxygenGenerator) * CalculateRating(lines, Rating::Co2Scrubber);
 }
 
 std::vector<std::string> ExtractLines(const std::string &text)
@@ -34,7 +47,7 @@ std::vector<std::string> ExtractLines(const std::string &text)
     return lines;
 }
 
-int CalculateRate(const std::vector<std::string> &lines, const std::string& option)
+int CalculateRate(const std::vector<std::string> &lines, Rate option)
 {
     const int width = lines[0].length();
     std::vector<unsigned int> gammaRate(width, 0);
@@ -52,25 +65,74 @@ int CalculateRate(const std::vector<std::string> &lines, const std::string& opti
             gammaRate[i] = 0;
     }
 
-    int decimalGammaRate = 0;
-    int j = 0;
-    for (auto i = gammaRate.rbegin(); i != gammaRate.rend(); ++i)
+    int decimalGammaRate = BinaryToDecimal(gammaRate);
+
+    switch (option)
     {
-        decimalGammaRate += (*i << j);
+    case Rate::Gamma:
+        return decimalGammaRate;
+
+    case Rate::Epsilon:
+        return (~decimalGammaRate) & ((1 << gammaRate.size()) - 1);
+
+    default:
+        std::cerr << "Option '" << static_cast<int>(option) << "' is not allowed\n";
+        return 0;
+    }
+}
+
+int BinaryToDecimal(const std::vector<unsigned int>& binaryVector)
+{
+    int decimalValue = 0;
+    int j = 0;
+    for (auto i = binaryVector.rbegin(); i != binaryVector.rend(); i++)
+    {
+        decimalValue += (*i << j);
         j++;
     }
+    return decimalValue;
+}
 
-    if (option == "gamma")
-        return decimalGammaRate;
-    else if (option == "epsilon")
-        return (~decimalGammaRate) & ((1 << gammaRate.size()) - 1);
-    std::cerr << "Option '" << option << "' is not allowed\n";
-    return 0;
+int CalculateRating(const std::vector<std::string> &lines, Rating option)
+{
+    std::vector<std::string> linesCopy = lines;
+    auto bitCount = CountBits(linesCopy);
+    char charSelected = '0';
+
+    if (bitCount[0] >= linesCopy.size() / 2)
+        charSelected = '1';
+
+    
+
+    switch (option)
+    {
+    case Rating::OxygenGenerator:
+        return 1;
+
+    case Rating::Co2Scrubber:
+        return 1;
+
+    default:
+        std::cerr << "Option '" << static_cast<int>(option) << "' is not allowed\n";
+        return 0;
+    }
+}
+
+std::vector<int> CountBits(const std::vector<std::string> &lines)
+{
+    int width = lines[0].length();
+    std::vector<int> bitCount(width, 0);
+    for (auto line : lines)
+        for (int i = 0; i < width; i++)
+            if (line[i] == '1')
+                bitCount[i]++;
+
+    return bitCount;
 }
 
 int main()
 {
-    std::string text = ReadTextFile("03/data/input.txt");
+    std::string text = ReadTextFile("03/data/test.txt");
     auto lines = ExtractLines(text);
 
     std::cout << "Part 1: " << Part1(lines) << "\n";
