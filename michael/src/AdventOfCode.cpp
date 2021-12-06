9648678c0d639002c5ec67f0ac30cc1eb1d04245
Michael Otty <michael.otty@gmail.com>
2021-12-06 18:46:16 +0000

    Main function moved to one object

--- michael/src/AdventOfCode.cpp
+++ michael/src/AdventOfCode.cpp
@@ -1,11 +1,49 @@
-#include "Days.h"
+#include <exception>
+#include <iostream>
+#include <string>
+#include <vector>
 
-int main()
+#include "Days.h"
+#include "Utilities.h"
+
+int main(int argc, char** argv)
 {
-    Day1();
-    Day2();
-    Day3();
-    Day4();
-    Day5();
-    Day6();
+    std::vector<const char*> args;
+    for (int i = 0; i < argc; i++)
+        args.push_back(argv[i]);
+
+    if (args.size() != 3)
+        throw std::invalid_argument("Wrong number of args");
+
+    int dayId = std::atoi(args[1]);
+    switch (dayId)
+    {
+    case 1:
+        Day1(args[2]);
+        break;
+
+    case 2:
+        Day2(args[2]);
+        break;
+
+    case 3:
+        Day3(args[2]);
+        break;
+
+    case 4:
+        Day4(args[2]);
+        break;
+
+    case 5:
+        Day5(args[2]);
+        break;
+
+    case 6:
+        Day6(args[2]);
+        break;
+
+    default:
+        throw std::invalid_argument(args[1]);
+        break;
+    }
 }
