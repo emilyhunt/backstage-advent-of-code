75950804bb789d4011dcd18e7843f1aabe84e9f9
Michael Otty <michael.otty@gmail.com>
2021-12-13 00:59:32 +0000

    Print file

---
+++ michael/src/12.cpp
@@ -0,0 +1,42 @@
+/**
+ * @file 12.cpp
+ * @author Michael Otty (michael.otty@gmail.com)
+ * @brief Advent of Code 2021 day 12
+ * @version 1.0.0
+ * @date 2021-12-13
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "Days.h"
+#include "Utilities.h"
+
+/*
+================================================================================
+                            Function Declarations
+================================================================================
+*/
+
+/*
+================================================================================
+                            Function Definitions
+================================================================================
+*/
+
+/**
+ * @brief Day 12 of Advent of Code
+ *
+ * @param fileName to read as puzzle input
+ */
+void Day12(const char* fileName)
+{
+    const std::string text = ReadTextFile(fileName);
+    std::cout << text << "\n";
+
+    // std::cout << "Part 1: " << Part1(data) << "\n";
+}
