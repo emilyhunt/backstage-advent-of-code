289fe9cc1f97b6ffb364e801715a2559bf85afcb
Fozzy <michaelotty@users.noreply.github.com>
2021-12-19 23:39:35 +0000

    Simplified michael's main function (#24)
    * Allow 2 args for default using input.txt
    
    * Using unordered_map to call function over switch
    
    * Add const to map

--- michael/src/AdventOfCode.cpp
+++ michael/src/AdventOfCode.cpp
@@ -10,6 +10,7 @@
  */
 
 #include <exception>
+#include <unordered_map>
 
 #include "Days.h"
 #include "Utilities.h"
@@ -22,119 +23,35 @@
  * @param argv Arguments, first is always the program itself
  * @return int Return 0 for normal exit
  */
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-    if (argc < 3)
+    if (argc < 2)
         throw std::invalid_argument(
             "Too few args, format is ./AdventOfCode <dayId> <inputFile>");
     if (argc > 3)
         throw std::invalid_argument(
             "Too many args, format is ./AdventOfCode <dayId> <inputFile>");
 
-    switch (std::atoi(argv[1]))
+    const auto select = std::atoi(argv[1]);
+
+    char fileName[32] = {};
+    if (argc == 2)
     {
-    case 1:
-        Day01(argv[2]);
-        break;
-
-    case 2:
-        Day02(argv[2]);
-        break;
-
-    case 3:
-        Day03(argv[2]);
-        break;
-
-    case 4:
-        Day04(argv[2]);
-        break;
-
-    case 5:
-        Day05(argv[2]);
-        break;
-
-    case 6:
-        Day06(argv[2]);
-        break;
-
-    case 7:
-        Day07(argv[2]);
-        break;
-
-    case 8:
-        Day08(argv[2]);
-        break;
-
-    case 9:
-        Day09(argv[2]);
-        break;
-
-    case 10:
-        Day10(argv[2]);
-        break;
-
-    case 11:
-        Day11(argv[2]);
-        break;
-
-    case 12:
-        Day12(argv[2]);
-        break;
-
-    case 13:
-        Day13(argv[2]);
-        break;
-
-    case 14:
-        Day14(argv[2]);
-        break;
-
-    case 15:
-        Day15(argv[2]);
-        break;
-
-    case 16:
-        Day16(argv[2]);
-        break;
-
-    case 17:
-        Day17(argv[2]);
-        break;
-
-    case 18:
-        Day18(argv[2]);
-        break;
-
-    case 19:
-        Day19(argv[2]);
-        break;
-
-    case 20:
-        Day20(argv[2]);
-        break;
-
-    case 21:
-        Day21(argv[2]);
-        break;
-
-    case 22:
-        Day22(argv[2]);
-        break;
-
-    case 23:
-        Day23(argv[2]);
-        break;
-
-    case 24:
-        Day24(argv[2]);
-        break;
-
-    case 25:
-        Day25(argv[2]);
-        break;
-
-    default:
-        throw std::invalid_argument(argv[1]);
-        break;
+        const char *fileNameBase = "data/xx/input.txt";
+        std::strcpy(fileName, fileNameBase);
+        fileName[5] = (select / 10) + '0';
+        fileName[6] = (select % 10) + '0';
     }
+    else
+        std::strcpy(fileName, argv[2]);
+
+    const std::unordered_map<int, void (*)(const char *)> functionMap{
+    {1, Day01}, {2, Day02}, {3, Day03}, {4, Day04}, {5, Day05}, {6, Day06},
+    {7, Day07}, {8, Day08}, {9, Day09}, {10, Day10}, {11, Day11}, {12, Day12},
+    {13, Day13}, {14, Day14}, {15, Day15}, {16, Day16}, {17, Day17},
+    {18, Day18}, {19, Day19}, {20, Day20}, {21, Day21}, {22, Day22},
+    {23, Day23}, {24, Day24}, {25, Day25}};
+
+    functionMap.at(select)(fileName);
+    return 0;
 }
