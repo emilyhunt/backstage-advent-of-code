499aa1fc6e978a71ce5791ef6f011a962e9faa41
Michael Otty <michael.otty@gmail.com>
2021-12-20 22:28:51 +0000

    Allowed the debugger with long file path work

--- michael/src/AdventOfCode.cpp
+++ michael/src/AdventOfCode.cpp
@@ -23,7 +23,7 @@
  * @param argv Arguments, first is always the program itself
  * @return int Return 0 for normal exit
  */
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
     if (argc < 2)
         throw std::invalid_argument(
@@ -34,23 +34,26 @@ int main(int argc, char **argv)
 
     const auto select = std::atoi(argv[1]);
 
-    char fileName[32] = {};
+    const size_t MAX_FILENAME_LEN = 256;
+    char fileName[MAX_FILENAME_LEN] = {};
     if (argc == 2)
     {
-        const char *fileNameBase = "data/xx/input.txt";
+        const char* fileNameBase = "data/xx/input.txt";
         std::strcpy(fileName, fileNameBase);
         fileName[5] = (select / 10) + '0';
         fileName[6] = (select % 10) + '0';
     }
+    else if (std::strlen(argv[2]) > (MAX_FILENAME_LEN - 1))
+        throw std::length_error("Input file path too long");
     else
         std::strcpy(fileName, argv[2]);
 
-    const std::unordered_map<int, void (*)(const char *)> functionMap{
-    {1, Day01}, {2, Day02}, {3, Day03}, {4, Day04}, {5, Day05}, {6, Day06},
-    {7, Day07}, {8, Day08}, {9, Day09}, {10, Day10}, {11, Day11}, {12, Day12},
-    {13, Day13}, {14, Day14}, {15, Day15}, {16, Day16}, {17, Day17},
-    {18, Day18}, {19, Day19}, {20, Day20}, {21, Day21}, {22, Day22},
-    {23, Day23}, {24, Day24}, {25, Day25}};
+    const std::unordered_map<int, void (*)(const char*)> functionMap{
+        {1, Day01},  {2, Day02},  {3, Day03},  {4, Day04},  {5, Day05},
+        {6, Day06},  {7, Day07},  {8, Day08},  {9, Day09},  {10, Day10},
+        {11, Day11}, {12, Day12}, {13, Day13}, {14, Day14}, {15, Day15},
+        {16, Day16}, {17, Day17}, {18, Day18}, {19, Day19}, {20, Day20},
+        {21, Day21}, {22, Day22}, {23, Day23}, {24, Day24}, {25, Day25}};
 
     functionMap.at(select)(fileName);
     return 0;
