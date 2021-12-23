e35f4f1d73dad0660bea8bc247afe69baf0ffe33
Michael Otty <michael.otty@gmail.com>
2021-12-23 19:25:38 +0000

    Solution to part 1

--- michael/src/23.cpp
+++ michael/src/23.cpp
@@ -9,6 +9,7 @@
  *
  */
 
+#include <array>
 #include <iostream>
 #include <string>
 
@@ -17,15 +18,37 @@
 
 /*
 ================================================================================
-                                Classes
-================================================================================
-*/
-
-/*
-================================================================================
                             Function Definitions
 ================================================================================
 */
+
+/**
+ * @brief Solution to part 1, solved by hand
+ *
+ * @return int Solution
+ */
+int Part1()
+{
+    const int A_COST = 1;
+    const int B_COST = 10;
+    const int C_COST = 100;
+    const int D_COST = 1000;
+    int answer = 0;
+
+    answer += 2 * D_COST;
+    answer += 3 * A_COST;
+    answer += 3 * D_COST;
+    answer += 6 * A_COST;
+    answer += 5 * D_COST;
+    answer += 5 * C_COST;
+    answer += 5 * C_COST;
+    answer += 5 * B_COST;
+    answer += 5 * B_COST;
+    answer += 3 * A_COST;
+    answer += 8 * A_COST;
+
+    return answer;
+}
 
 /**
  * @brief Day 23 of Advent of Code
@@ -35,5 +58,7 @@
 void Day23(const char* fileName)
 {
     const std::string text = ReadTextFile(fileName);
-    std::cout << text << "\n";
+    std::cout << "Puzzle:\n" << text << "\n\n";
+
+    std::cout << "Part 1: " << Part1() << "\n";
 }
