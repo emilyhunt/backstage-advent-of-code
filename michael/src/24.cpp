e01c89b2d7033f7128e31ecf9946046a152e1f5d
Michael Otty <michael.otty@gmail.com>
2022-05-02 14:37:54 +0100

    Clang-format

--- michael/src/24.cpp
+++ michael/src/24.cpp
@@ -11,8 +11,8 @@
 
 #include <iostream>
 #include <string>
-#include <vector>
 #include <utility>
+#include <vector>
 
 #include "Days.h"
 #include "Utilities.h"
@@ -31,36 +31,34 @@
 
 using Instructions = std::vector<std::pair<std::string, std::string>>;
 
-static Instructions ExtractInstructions(const std::string &text)
+static Instructions ExtractInstructions(const std::string& text)
 {
     Instructions instructions;
     auto lines = SplitLines(text);
-    for (const auto &line : lines)
-        instructions.push_back(std::make_pair(line.substr(0, 3), line.substr(4)));
+    for (const auto& line : lines)
+        instructions.push_back(
+            std::make_pair(line.substr(0, 3), line.substr(4)));
 
     return instructions;
 }
 
-static void PrintInstructions(const Instructions &instructions)
+static void PrintInstructions(const Instructions& instructions)
 {
-    for (const auto &instruction : instructions)
+    for (const auto& instruction : instructions)
     {
         std::cout << instruction.first << " ";
         std::cout << instruction.second << "\n";
     }
 }
 
-static int Part1(const Instructions &instructions)
-{
-    return 0;
-}
+static int Part1(const Instructions& instructions) { return 0; }
 
 /**
  * @brief Day 24 of Advent of Code
  *
  * @param fileName to read as puzzle input
  */
-void Day24(const char *fileName)
+void Day24(const char* fileName)
 {
     const auto text = ReadTextFile(fileName);
     auto instructions = ExtractInstructions(text);
